d1b9a148e1da814a9b1be89dfb67ea57b7490171
GuilhermeP27 <guiperozo@hotmail.com>
2021-02-26 14:57:41 -0300

    Primeiro Commit

---
+++ CRUD/CRUD/Controllers/AulaMDController.cs
@@ -0,0 +1,84 @@
+﻿using CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class AulaMDController : Controller
+    {
+        // GET: AulaMD
+        public ActionResult Index()
+        {
+            AULA_MDEntities ctx = new AULA_MDEntities();
+            var listaAula = ctx.AULA_MD.ToList();
+            return View(listaAula);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(AULA_MD aula)
+        {
+            if (ModelState.IsValid)
+            {
+                AULA_MDEntities ctx = new AULA_MDEntities();
+                ctx.AULA_MD.Add(aula);
+                ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        
+        public ActionResult Delete(int id)
+        {
+            AULA_MDEntities ctx = new AULA_MDEntities();
+            var excluirAula = ctx.AULA_MD.Find(id);
+            return View(excluirAula);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(AULA_MD aula)
+        {
+            AULA_MDEntities ctx = new AULA_MDEntities();
+            var excluirAula = ctx.AULA_MD.Find(aula.AulaId);
+            ctx.AULA_MD.Remove(excluirAula);
+            ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            AULA_MDEntities ctx = new AULA_MDEntities();
+            var editarAula = ctx.AULA_MD.Find(id);
+            return View(editarAula);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(AULA_MD aula)
+        {
+            if (ModelState.IsValid)
+            {
+                AULA_MDEntities ctx = new AULA_MDEntities();
+                ctx.Entry(aula).State = System.Data.Entity.EntityState.Modified;
+                ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            AULA_MDEntities ctx = new AULA_MDEntities();
+            var detalheAula = ctx.AULA_MD.Find(id);
+            return View(detalheAula);
+        }
+
+
+    }
+}
---
+++ CRUD/CRUD/MDT/AULA_MDT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    [MetadataType(typeof(AULA_MDT))]
+    public partial class AULA_MD{}
+
+    public class AULA_MDT
+    {
+        [DisplayName("Código")]
+        public int AulaId { get; set; }
+        [DisplayName("Descrição")]
+        [Required(ErrorMessage ="Campo Obrigatório.")]
+        [StringLength(9, MinimumLength =4,ErrorMessage ="Minímo de 4 caracteres e no máximo de 9 caracteres.")]
+        public string AulaDescricao { get; set; }
+    }
+}
